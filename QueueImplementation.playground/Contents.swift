76694c5e40a6b3d015a77ef56ea8d08b70de0512
Heber Raziel Alvarez Ruedas <heber@Hebers-MacBook-Pro.local>
2024-02-01 17:17:36 -0600

    QueueImplementation - Add methods * Peak * Enqueue * Dequeue * isFull * isEmpty

--- QueueImplementation.playground/Contents.swift
+++ QueueImplementation.playground/Contents.swift
@@ -1,3 +1,70 @@
 import UIKit
 
-var greeting = "Hello, playground"
+class MyQueue {
+    var queueLength = 3
+    var items = [Int](repeating: .zero, count: 3)
+    var front = -1
+    var back = -1
+
+    var empty: Bool {
+        front == -1 && back == -1
+    }
+
+    var isFull: Bool {
+        back == queueLength - 1
+    }
+
+    func enQueue(itemValue: Int) {
+        if isFull {
+            print("Queue is full")
+        } else if empty {
+            back = 0
+            front = 0
+            items[back] = itemValue
+        } else {
+            back += 1
+            items[back] = itemValue
+        }
+    }
+
+    func deQueue() {
+        if empty {
+            print("Queue is empty, nothing to dequeue")
+        } else if front == back {
+            front = -1
+            back = -1
+        } else {
+            front += 1
+        }
+    }
+
+    func display() {
+        let i = 0
+        
+        if empty {
+            print("Queue is empty")
+        } else {
+            for i in front..<back {
+                print(items[i])
+            }
+        }
+    }
+
+    func peek() {
+        print("Front value is \(items[front])")
+    }
+}
+
+func main() {
+    let myQueue = MyQueue()
+    
+    myQueue.enQueue(itemValue: 3)
+    myQueue.enQueue(itemValue: 2)
+    myQueue.enQueue(itemValue: 1)
+    
+    myQueue.display()
+    
+    myQueue.peek()
+}
+
+main()
